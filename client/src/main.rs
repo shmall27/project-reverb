1a03831f425fdb0fad8c28d90e26b8b2407f3dea
Sam Hall <samuel27hall@gmail.com>
2023-05-25 08:28:51 -0700

    Added network update utilities

--- client/src/main.rs
+++ client/src/main.rs
@@ -9,18 +9,17 @@ mod utils;
 
 use crossterm::{
     event::{poll, read, Event, KeyCode},
-    terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType},
-    execute
+    terminal::{disable_raw_mode, enable_raw_mode},
 };
 use rand::Rng;
 use std::time::Duration;
-use std::io::stdout;
 
-use std::net::{UdpSocket};
 use std::error::Error;
+use std::net::UdpSocket;
+
+use std::ptr;
 use std::sync::{mpsc, Arc};
 use std::thread;
-use std::ptr;
 
 use sdl2::pixels::PixelFormatEnum;
 // use sdl2::rect::Rect;
@@ -34,9 +33,9 @@ struct FrameData {
 
 struct Decoder {
     buf: Box<[u8]>,
-    packet: * mut AVPacket,
-    frame: * mut AVFrame,
-    codecContext: * mut AVCodecContext,
+    packet: *mut AVPacket,
+    frame: *mut AVFrame,
+    codecContext: *mut AVCodecContext,
 }
 
 impl Decoder {
@@ -52,7 +51,7 @@ impl Decoder {
         }
 
         let empty_dict_ptr: *mut *mut AVDictionary = ptr::null_mut();
-        let ret = avcodec_open2(codec_context, video_codec,empty_dict_ptr);
+        let ret = avcodec_open2(codec_context, video_codec, empty_dict_ptr);
         if ret < 0 {
             return Err(format!("Failed to open codec: {}", ret).into());
         }
@@ -78,7 +77,10 @@ impl Decoder {
         })
     }
 
-    unsafe fn decode_buffer_arr(&mut self, buffer: &[u8]) -> Result<Vec<FrameData>, Box<dyn Error>>{
+    unsafe fn decode_buffer_arr(
+        &mut self,
+        buffer: &[u8],
+    ) -> Result<Vec<FrameData>, Box<dyn Error>> {
         self.buf[..buffer.len()].copy_from_slice(buffer);
 
         (*self.packet).data = self.buf.as_mut_ptr();
@@ -105,9 +107,16 @@ impl Decoder {
                 break;
             }
 
-            let plane_height = if i == 0 { frame_height } else { frame_height / 2 };
+            let plane_height = if i == 0 {
+                frame_height
+            } else {
+                frame_height / 2
+            };
             let plane_data = std::slice::from_raw_parts(data_ptr, linesize * plane_height);
-            planes.push(FrameData {data: plane_data.to_vec(), linesize: linesize});
+            planes.push(FrameData {
+                data: plane_data.to_vec(),
+                linesize: linesize,
+            });
         }
 
         return Ok(planes);
@@ -122,52 +131,63 @@ impl Decoder {
     }
 }
 
+const CONNECTION_REQUEST: [u8; 4] = [1, 0, 1, 0];
+const NETWORK_STATE_UPDATE: [u8; 4] = [1, 1, 0, 0];
+
 fn main() {
-    let socket = UdpSocket::bind("127.0.0.1:6000").unwrap();
+    let socket = UdpSocket::bind("0.0.0.0:6000").unwrap();
     let thread_socket = socket.try_clone().unwrap();
-    let mut buf = [0; 600];
+    let mut buf = [0; 604];
     let (tx, rx) = mpsc::channel::<[u8; 600]>();
 
-    // remove after testing
-    let mut network_state = utils::peer_tree::PeerTree::new(utils::peer_tree::PeerNode {connections_offset: 0, max_connections: 3});
-    enable_raw_mode().unwrap();
+    // this is only if you're the host
+    let mut network_state = utils::peer_tree::PeerTree::new();
 
-    loop {
-        if poll(Duration::from_millis(100)).unwrap() {
-            if let Event::Key(key_event) = read().unwrap() {
-                if key_event.code == KeyCode::Char(' ') {
-                    let mut rng = rand::thread_rng();
-                    let max_connections: i32 = rng.gen_range(1..50);
+    let mut diff_map: Vec<u8> = Vec::new();
 
-                    let new_peer = utils::peer_tree::PeerNode {
-                        connections_offset: 0,
-                        max_connections,
-                    };
+    thread::spawn(move || loop {
+        let (num_bytes, src) = socket.recv_from(&mut buf).unwrap();
 
-                    network_state.insert_peer(new_peer);
+        let connection_type = buf[..4].to_vec();
 
-                    // network_state.pretty_print();
-                } else if key_event.code == KeyCode::Esc {
-                    break;
+        if connection_type == CONNECTION_REQUEST {
+            // connection request can only be made to the host
+            let max_connections = buf[0];
+
+            let network_changes = network_state.insert_peer(utils::peer_tree::PeerNode {
+                ip: src,
+                connections_offset: 0,
+                max_connections: max_connections.try_into().unwrap(),
+            });
+
+            let mut cursor = 4; // start after connection_type
+            for change in network_changes {
+                let change_bytes = change.convert_to_bytes();
+                if cursor + change_bytes.len() > buf.len() {
+                    panic!("network_changes size exceeded buf size");
                 }
+                buf[cursor..cursor + change_bytes.len()].copy_from_slice(&change_bytes[..]);
+                cursor += change_bytes.len();
             }
+        } else if connection_type == NETWORK_STATE_UPDATE {
+            // TODO: parse the message and update the network state
+        } else {
+            println!("{:?}", &buf[0..600]);
+            tx.send(buf[0..600].try_into().unwrap()).unwrap();
         }
-    }
-
-    disable_raw_mode().unwrap();
-    // end remove
-
-    thread::spawn(move || {
-        loop {
-            socket.recv_from(&mut buf).unwrap();
-            tx.send(buf).unwrap();
+        for peer in &network_state.data[0].data {
+            socket
+                // convert diff_map to same type as buf
+                .send_to(&buf, peer.ip)
+                .expect("Failed to send connection request");
         }
     });
 
     let (width_height_sender, width_height_receiver) = mpsc::channel::<(u32, u32)>();
 
     // create the thread-safe queue within the arc here
-    let shared_queue = Arc::new(utils::thread_safe_queue::ThreadSafeQueue::<Vec<FrameData>>::new(300));
+    let shared_queue =
+        Arc::new(utils::thread_safe_queue::ThreadSafeQueue::<Vec<FrameData>>::new(300));
     // clone it and pass to the decoder thread
     let decoder_queue = shared_queue.clone();
     thread::spawn(move || {
@@ -182,7 +202,9 @@ fn main() {
                     if is_first {
                         let frame_width = unsafe { decoder.width() as u32 };
                         let frame_height = unsafe { decoder.height() as u32 };
-                        width_height_sender.send((frame_width, frame_height)).unwrap();
+                        width_height_sender
+                            .send((frame_width, frame_height))
+                            .unwrap();
                         is_first = false;
                     }
                     decoder_queue.push(frame_data);
@@ -191,15 +213,12 @@ fn main() {
                     println!("Error: {}", e);
                 }
             }
-
-            //send the message to a new udp connection
-            thread_socket.send_to(&message, "127.0.0.1:4242").unwrap();
         }
     });
 
     // clone it and pass to the sdl thread
     let display_queue = shared_queue.clone();
-    
+
     let (frame_width, frame_height) = width_height_receiver.recv().unwrap();
 
     let sdl_context = sdl2::init().unwrap();
@@ -211,7 +230,6 @@ fn main() {
         .opengl()
         .build()
         .unwrap();
-    
 
     let mut canvas = window.into_canvas().build().unwrap();
 
@@ -220,7 +238,7 @@ fn main() {
     let mut video_texture = texture_creator
         .create_texture_streaming(PixelFormatEnum::YV12, frame_width, frame_height)
         .unwrap();
-    
+
     canvas.clear();
     canvas.present();
     loop {
@@ -231,12 +249,12 @@ fn main() {
         for (i, plane) in planes.iter().enumerate() {
             let plane_slice = &plane.data[..];
             let plane_linesize = plane.linesize;
-            
+
             let rect = match i {
                 0 => sdl2::rect::Rect::new(0, 0, frame_width, frame_height),
                 _ => sdl2::rect::Rect::new(0, 0, frame_width / 2, frame_height / 2),
             };
-            
+
             video_texture
                 .update(rect, plane_slice, plane_linesize)
                 .unwrap();
--- client/src/utils/peer_tree.rs
+++ client/src/utils/peer_tree.rs
@@ -1,37 +1,69 @@
 use std::ops::Range;
+use std::net::{IpAddr, SocketAddr};
 
 #[derive(Debug, Copy, Clone)]
 pub struct PeerNode {
-    pub connections_offset: i32,
-    pub max_connections: i32,
+    pub ip: SocketAddr,
+    pub connections_offset: u8,
+    pub max_connections: u8,
 }
 
 #[derive(Debug, Clone)]
 pub struct PeerLevel {
-    data: Vec<PeerNode>,
-    max_connections: i32,
+    pub data: Vec<PeerNode>,
+    pub max_connections: u8,
 }
 
 pub struct PeerTree {
     pub data: Vec<PeerLevel>
 }
 
-impl PeerTree {
-    pub fn new(peer: PeerNode) -> Self {
-        let level = PeerLevel {
-            data: vec![peer],
-            max_connections: peer.max_connections,
+#[derive(Debug)]
+
+pub struct PeerLocation {
+    breadth: u8,
+    depth: u8,
+}
+
+#[derive(Debug)]
+pub struct PeerWithLocation {
+    peer: PeerNode,
+    location: PeerLocation,
+}
+
+impl PeerWithLocation {
+    pub fn convert_to_bytes(&self) -> Vec<u8> {
+        let mut peer_bytes = Vec::new();
+
+        let mut ip_bytes = match self.peer.ip.ip() {
+            IpAddr::V4(ip) => ip.octets().to_vec(),
+            IpAddr::V6(ip) => ip.octets().to_vec(),
         };
 
+        let port_bytes = self.peer.ip.port().to_be_bytes().to_vec();
+
+        ip_bytes.extend(port_bytes);
+
+        peer_bytes.extend(ip_bytes);
+        peer_bytes.push(self.peer.connections_offset);
+        peer_bytes.push(self.peer.max_connections);
+        peer_bytes.push(self.location.breadth);
+        peer_bytes.push(self.location.depth);
+
+        return peer_bytes;
+    }
+}
+
+impl PeerTree {
+    pub fn new() -> Self {
         Self {
-            data: vec![level]
+            data: Vec::new()
         }
     }
 
 
     fn _breadth_search(level: &mut PeerLevel, range: Range<usize>, peer: &PeerNode) -> usize {  
-        // maybe it doesn't like finding the item at the end of the array
-        let mid_point = range.start + ((range.end - range.start) / 2);
+        let mid_point = (range.start + range.end) / 2;
     
         if range.start >= range.end {
             return mid_point;
@@ -72,31 +104,35 @@ impl PeerTree {
         return (*depth, breadth);
     }
 
-    pub fn insert_peer(&mut self, peer: PeerNode) {
+    pub fn insert_peer(&mut self, peer: PeerNode) -> Vec<PeerWithLocation>{
         let mut depth = 0;
+        let mut insert_diff: Vec<PeerWithLocation> = Vec::new();
         let dimensions = Self::_depth_search(self, &peer, &mut depth);
         self.data[dimensions.0].max_connections += peer.max_connections;
         self.data[dimensions.0].data.insert(dimensions.1, peer);
+        // need to accumulate the overall number of recursive changes to pass to the next peer
+        // in most scenarios, there won't need to be recursive calls
+        // if there are, it will probably be just one or two
+
 
         // remove weakest peer if the level is full
         if dimensions.0 != 0 && self.data[dimensions.0 - 1].max_connections < self.data[dimensions.0].data.len().try_into().unwrap() {
             let weakest_peer = self.data[dimensions.0].data.remove(0);
             self.data[dimensions.0].max_connections -= weakest_peer.max_connections;
-            
-            // there's an error when reinserting the weakest peer that ends up being the stronger peer in the next level
-            println!("Trying to insert peer: {:?}", peer);
-            println!("Reinserting weakest_peer: {:?}", weakest_peer);
-            self.pretty_print();
 
-            self.insert_peer(weakest_peer);
+            insert_diff.extend(self.insert_peer(weakest_peer));
         }
+        return insert_diff;
+    }
+
+    pub fn easy_insert_peer(&mut self, peer: PeerNode, location: PeerLocation) {
+        self.data[location.depth as usize].data.insert(location.breadth as usize, peer);
     }
 
     pub fn pretty_print(&self) {
         for level in &self.data {
-            let level_connections: Vec<i32> = level.data.iter().map(|peer| peer.max_connections).collect();
+            let level_connections: Vec<u8> = level.data.iter().map(|peer| peer.max_connections).collect();
             println!("{:?}", level_connections);
         }
     }
 }
-
